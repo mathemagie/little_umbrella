dbf50e4a93e09e5b2b354ade51f07798b8ee02ff
mathemagie <aurelien.fache@gmail.com>
2013-09-13 14:53:35 +0200

    right include path

--- cron/get_weeather.php
+++ cron/get_weeather.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../client_js/pusher-php-server-master/lib/Pusher.php';
+require_once '/home/dev/mathemagie.net/www/little_umbrella/client_js/pusher-php-server-master/lib/Pusher.php';
 
 $app_id = '34650';
 $app_key = 'b001467f86773130f94c';
@@ -47,7 +47,9 @@ foreach (array_keys($status) as $st) {
 	
 }
 echo "status :" . $right_status . "\n";
+//$right_status= 0;
 $pusher = new Pusher( $app_key, $app_secret, $app_id );
+//$pusher->trigger( 'test_channel', 'my_event', '' );
 if (!$right_status) {
 	$pusher->trigger( 'test_channel', 'close_umbrella', '' );
 }else {
